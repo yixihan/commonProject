2f729f6ba431fdee5722a977381d59eda82c9df1
zengsitong <zengsitong@newhope.cn>
2022-10-16 15:43:45 +0800

    22-10-16

---
+++ leetcode/leetcode/editor/cn/[886]可能的二分法.go
@@ -0,0 +1,108 @@
+package main
+
+//给定一组 n 人（编号为 1, 2, ..., n）， 我们想把每个人分进任意大小的两组。每个人都可能不喜欢其他人，那么他们不应该属于同一组。
+//
+// 给定整数 n 和数组 dislikes ，其中 dislikes[i] = [ai, bi] ，表示不允许将编号为 ai 和 bi的人归入同一组。当可以用
+//这种方法将所有人分进两组时，返回 true；否则返回 false。 
+//
+// 
+//
+// 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 4, dislikes = [[1,2],[1,3],[2,4]]
+//输出：true
+//解释：group1 [1,4], group2 [2,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 3, dislikes = [[1,2],[1,3],[2,3]]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：n = 5, dislikes = [[1,2],[2,3],[3,4],[4,5],[1,5]]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 2000 
+// 0 <= dislikes.length <= 10⁴ 
+// dislikes[i].length == 2 
+// 1 <= dislikes[i][j] <= n 
+// ai < bi 
+// dislikes 中每一组都 不同 
+// 
+//
+// 
+//
+// Related Topics 深度优先搜索 广度优先搜索 并查集 图 👍 270 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func possibleBipartition(n int, dislikes [][]int) bool {
+	graph := make([][]int, n)
+	group := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		graph[i] = make ([]int, n)
+	}
+
+	for _, d := range dislikes {
+		graph[d[0] - 1][d[1] - 1] = 1
+		graph[d[1] - 1][d[0] - 1] = 1
+	}
+
+	for i := 0; i < n; i++ {
+		if group[i] == 0 && dfs (graph, group, i, 1) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func dfs (graph [][]int, group []int, index, g int) bool {
+	group[index] = g
+
+	for i := 0; i < len(graph); i++ {
+		if graph[index][i] == 1 {
+			if group[i] == g {
+				return true
+			}
+			if group[i] == 0 && dfs (graph, group, i, -g) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+/*
+private boolean dfs(int[][] graph, int[] group, int index, int g) {
+	group[index] = g;
+	for (int i = 0; i < graph.length; i++) {
+		if (graph[index][i] == 1) {
+			if (group[i] == g) {
+				return true;
+			}
+			if (group[i] == 0 && dfs (graph, group, i, -g)) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+ */
+//leetcode submit region end(Prohibit modification and deletion)
